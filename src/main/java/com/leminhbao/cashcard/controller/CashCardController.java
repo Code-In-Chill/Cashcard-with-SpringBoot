351c6ea54589d4c70bac26a06ef674f7990fe18f
Lê Minh Bảo <besilent308@gmail.com>
2023-12-20 16:42:40 +0700

    Change to use Repository

--- src/main/java/com/leminhbao/cashcard/controller/CashCardController.java
+++ src/main/java/com/leminhbao/cashcard/controller/CashCardController.java
@@ -1,24 +1,31 @@
 package com.leminhbao.cashcard.controller;
 
 import com.leminhbao.cashcard.entity.CashCard;
+import com.leminhbao.cashcard.repository.CashCardRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/cashcards")
 public class CashCardController {
+    private final CashCardRepository cashCardRepository;
+
+    private CashCardController(CashCardRepository cashCardRepository) {
+        this.cashCardRepository = cashCardRepository;
+    }
 
     @GetMapping("/{requestedId}")
     private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-        CashCard cashCard = new CashCard(99L, 123.45);
-        if (requestedId.equals(cashCard.id())) {
-            return ResponseEntity.ok(cashCard);
+        Optional<CashCard> cashCardOptional = cashCardRepository.findById(requestedId);
+        if (cashCardOptional.isPresent()) {
+            return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
     }
-
 }
---
+++ src/main/java/com/leminhbao/cashcard/repository/CashCardRepository.java
@@ -0,0 +1,7 @@
+package com.leminhbao.cashcard.repository;
+
+import com.leminhbao.cashcard.entity.CashCard;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CashCardRepository extends CrudRepository<CashCard, Long> {
+}
