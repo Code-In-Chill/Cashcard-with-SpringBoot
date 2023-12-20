95c89701ac00f92f786fba6f8a0305eca7d9f84c
Lê Minh Bảo <besilent308@gmail.com>
2023-12-20 10:41:33 +0700

    Initial commit

---
+++ src/test/java/com/leminhbao/cashcard/CashCardJsonTest.java
@@ -0,0 +1,13 @@
+package com.leminhbao.cashcard;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CashCardJsonTest {
+
+    @Test
+    void myFirstTest() {
+        assertThat(1).isEqualTo(24);
+    }
+}
